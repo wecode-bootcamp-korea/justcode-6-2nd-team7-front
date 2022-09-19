ebfb2930c49599cbbe8e86356c7fbf44fa6f1b9b
zionii <tjwldnjs12345@naver.com>
2022-09-19 20:51:26 +0900

    Add: 체크박스 전체선택 구현

--- src/pages/SignUp/SignUp.js
+++ src/pages/SignUp/SignUp.js
@@ -1,7 +1,21 @@
-import React from 'react';
+import React, { useState } from 'react';
 import * as S from './SignUp.Styled';
 
 function SignUp() {
+  const [checkList, setCheckList] = useState([]);
+
+  // 체크박스 전체선택시 모두선택 체크박스 활성화시키기
+  const checkAll = (e) => {
+    e.target.checked ? setCheckList(['check1', 'check2', 'check3', 'check4', 'check5', 'check6']) : setCheckList([]);
+  };
+
+  // 전체체크 선택시 전체 선택 or 전체해제
+  const handleCheck = (e) => {
+    e.target.checked
+      ? setCheckList([...checkList, e.target.name])
+      : setCheckList(checkList.filter((el) => el !== e.target.name));
+  };
+
   return (
     <S.SignUpForm>
       <div className='signup-container'>
@@ -10,37 +24,67 @@ function SignUp() {
         </div>
         <p>
           <label>
-            <input type='checkbox' />
+            <input type='checkbox' checked={checkList.length === 6 ? true : false} onChange={checkAll} />
             <span>전체동의</span>
           </label>
         </p>
         <p>
-          <input type='checkbox' />
+          <input
+            type='checkbox'
+            name='check1'
+            checked={checkList.includes('check1') ? true : false}
+            onChange={handleCheck}
+          />
           <span className='underline'>이용약관 동의</span>
           <span className='essential'>(필수)</span>
         </p>
         <p>
-          <input type='checkbox' />
+          <input
+            type='checkbox'
+            name='check2'
+            checked={checkList.includes('check2') ? true : false}
+            onChange={handleCheck}
+          />
           <span className='underline'>만 14세 이상 확인 </span>
           <span className='essential'>(필수)</span>
         </p>
         <p>
-          <input type='checkbox' />
+          <input
+            type='checkbox'
+            name='check3'
+            checked={checkList.includes('check3') ? true : false}
+            onChange={handleCheck}
+          />
           <span className='underline'>개인정보 수집 및 이용 동의</span>
           <span className='essential'>(필수)</span>
         </p>
         <p>
-          <input type='checkbox' />
+          <input
+            type='checkbox'
+            name='check4'
+            checked={checkList.includes('check4') ? true : false}
+            onChange={handleCheck}
+          />
           <span className='underline'>개인정보 수집 및 이용 동의</span>
           <span>(선택)</span>
         </p>
         <p>
-          <input type='checkbox' />
+          <input
+            type='checkbox'
+            name='check5'
+            checked={checkList.includes('check5') ? true : false}
+            onChange={handleCheck}
+          />
           <span className='underline'>마케팅 알림 수신동의</span>
           <span>(선택)</span>
         </p>
         <p>
-          <input type='checkbox' />
+          <input
+            type='checkbox'
+            name='check6'
+            checked={checkList.includes('check6') ? true : false}
+            onChange={handleCheck}
+          />
           <span className='underline'> 위치기반 서비스 이용약관 동의</span>
           <span>(선택)</span>
         </p>
