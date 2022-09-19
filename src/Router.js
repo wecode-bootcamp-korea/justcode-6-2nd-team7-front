1f21fb8c14b9e17e84d27211118b78b3ff5b2ae2
zionii <tjwldnjs12345@naver.com>
2022-09-19 16:52:48 +0900

    Add:Login페이지  input,button 레이아웃

--- src/Router.js
+++ src/Router.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+import KaKaoLogin from './pages/Login/KaKaoLogin';
+import Login from './pages/Login/Login';
 import theme from './styles/theme';
 
 const Router = (props) => {
   return (
     <ThemeProvider theme={theme}>
       <BrowserRouter>
-        <Routes>{/* <Route path='' element={} /> */}</Routes>
+        <Routes>
+          {/* <Route path='' element={} /> */}
+          <Route path='/login' element={<Login />} />
+        </Routes>
       </BrowserRouter>
     </ThemeProvider>
   );
