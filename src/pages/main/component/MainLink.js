ee2378774db8039e0c69d5464bd480fc7124d2ed
YesunPark <lynn08082@gmail.com>
2022-09-21 01:45:49 +0900

    Modify: 숙소연결 링크태그로 변경

--- src/pages/main/component/MainLink.js
+++ src/pages/main/component/MainLink.js
@@ -1,23 +1,46 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import styled from 'styled-components';
 
 const MainLinkContainer = styled.div`
-  display: flex;
   width: 1024px;
   margin: auto;
+
+  .link-list {
+    display: flex;
+    justify-content: space-between;
+  }
 `;
 
 const MainLink = () => {
   return (
     <MainLinkContainer>
-      <div>모텔</div>
-      <div>호텔•리조트</div>
-      <div>펜션</div>
-      <div>게스트하우스</div>
-      <div>캠핑•글램핑</div>
-      <div>한옥</div>
-      <div>내주변</div>
-      <div>페이백</div>
+      <ul className='link-list'>
+        <li>
+          <Link>모텔</Link>
+        </li>
+        <li>
+          <Link>호텔•리조트</Link>
+        </li>
+        <li>
+          <Link>펜션</Link>
+        </li>
+        <li>
+          <Link>게스트하우스</Link>
+        </li>
+        <li>
+          <Link>캠핑•글램핑</Link>
+        </li>
+        <li>
+          <Link>한옥</Link>
+        </li>
+        <li>
+          <Link>내주변</Link>
+        </li>
+        <li>
+          <Link>페이백</Link>
+        </li>
+      </ul>
     </MainLinkContainer>
   );
 };
