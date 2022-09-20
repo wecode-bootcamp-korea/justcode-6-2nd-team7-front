eed5dca22ce35843674cf9ba8b50d6ac82a5ac07
YesunPark <lynn08082@gmail.com>
2022-09-20 21:28:23 +0900

    Add: 로고 클릭 시 메인화면으로 이동

--- src/component/nav/Nav.js
+++ src/component/nav/Nav.js
@@ -1,33 +1,47 @@
-import React from 'react';
+import { React, useState, useEffect } from 'react';
 import { Link } from 'react-router-dom';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import styled from 'styled-components';
 import * as S from './Nav.styled.js';
 
 const Nav = () => {
+  const [scrollPosition, setScrollPosition] = useState(0);
+  const updateScroll = () => {
+    setScrollPosition(window.scrollY || document.documentElement.scrollTop);
+  };
+  useEffect(() => {
+    window.addEventListener('scroll', updateScroll);
+  });
+
   return (
     <S.NavStyle>
-      <div className='nav-container'>
-        <img className='logo' src='/images/logoWhite.svg' alt='logo' />
-        <div className='nav-content'>
-          <li className='navigate-list'>
-            {/* <FontAwesomeIcon icon='fa-solid fa-magnifying-glass' /> */}
-            <ul>
-              <Link to=''>내주변</Link>
-            </ul>
-            <ul>
-              <Link to='/my'>예약내역</Link>
-            </ul>
-            <ul>
-              <Link to=''>더보기</Link>
-            </ul>
-            <ul>
-              {localStorage.getItem('token') !== null ? (
-                <Link to='/my'>마이페이지</Link>
-              ) : (
-                <Link to='/login'>로그인</Link>
-              )}
-            </ul>
-          </li>
+      <div className={scrollPosition < 2 ? 'nav' : 'nav-change'}>
+        {/* style={{ backgroundColor: scrollPosition < 1 ? ({ theme }) => theme.colors.mainColor : '#ffff' }} */}
+        <div className='nav-container'>
+          <Link to='/' className='logo-box'>
+            <img className='logo' src='/images/logoWhite.svg' alt='logo' />
+          </Link>
+          <div className='nav-content'>
+            <li className='navigate-list'>
+              {/* <FontAwesomeIcon icon='fa-solid fa-magnifying-glass' /> */}
+              <ul>
+                <Link to=''>내주변</Link>
+              </ul>
+              <ul>
+                <Link to='/my'>예약내역</Link>
+              </ul>
+              <ul>
+                <Link to=''>더보기</Link>
+              </ul>
+              <ul>
+                {localStorage.getItem('token') !== null ? (
+                  <Link to='/my'>마이페이지</Link>
+                ) : (
+                  <Link to='/login'>로그인</Link>
+                )}
+              </ul>
+            </li>
+          </div>
         </div>
       </div>
     </S.NavStyle>
--- src/component/nav/Nav.styled.js
+++ src/component/nav/Nav.styled.js
@@ -10,35 +10,51 @@ export const NavStyle = styled.nav`
   font-family: pretendard;
   z-index: 99;
 
-  .nav-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    width: 1024px;
+  /* .nav {
+    background-color: ${({ theme }) => theme.colors.mainColor};
+  }
+
+  .nav-change {
+    background-color: #ffff;
+    color: black;
+  } */
+  .nav {
     height: inherit;
-    margin: auto;
+    .nav-container {
+      display: flex;
+      justify-content: space-between;
+      align-items: center;
+      width: 1024px;
+      height: inherit;
+      margin: auto;
 
-    .navigate-list {
-      list-style-type: none;
-      ul {
-        float: left;
-        line-height: inherit;
-        a {
-          margin-left: 20px;
-          color: rgba(255, 255, 255, 0.767);
-          font-size: 18px;
-          text-decoration: none;
+      .navigate-list {
+        list-style-type: none;
+        ul {
+          float: left;
+          line-height: inherit;
+          a {
+            margin-left: 20px;
+            color: rgba(255, 255, 255, 0.767);
+            font-size: 18px;
+            text-decoration: none;
 
-          &:hover {
-            color: #ffff;
+            &:hover {
+              color: #ffff;
+            }
           }
         }
       }
     }
   }
 
-  .logo {
-    width: 96px;
-    height: 27px;
+  .logo-box {
+    display: flex;
+    height: inherit;
+    align-items: center;
+    .logo {
+      width: 96px;
+      height: 27px;
+    }
   }
 `;
