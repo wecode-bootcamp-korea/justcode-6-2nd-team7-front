053da22a6b784ccddd96da65ed4134ff37f10458
YesunPark <lynn08082@gmail.com>
2022-09-21 12:13:59 +0900

    Modify: index.scss 추가

--- src/styles/theme.js
+++ src/styles/theme.js
@@ -1,5 +1,12 @@
 export default {
   colors: {
     mainColor: '#01AEF0',
+    highRating: '#615fb8',
+    lowRating: '#94a0b2',
+    yellow: '#ffa726',
+    green: '#168d85',
+    bkText: '#303030',
+    text: '#7A7A7A',
+    greyBg: '#efefef',
   },
 };
