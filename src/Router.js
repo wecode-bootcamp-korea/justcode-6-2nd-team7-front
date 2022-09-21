f1616968cc5b0c33b093cb2d39e5fa51012c4ffd
YesunPark <108933466+YesunPark@users.noreply.github.com>
2022-09-21 22:09:44 +0900

    Add: footer UI 완성 (#6)

--- src/Router.js
+++ src/Router.js
@@ -1,15 +1,16 @@
 import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import { ThemeProvider } from 'styled-components';
+
+import Nav from './component/nav/Nav';
+import Main from './pages/main/Main';
 import Login from './pages/login/Login';
 import PhoneCheck from './pages/signup/PhoneCheck';
 import SignUp from './pages/signup/SignUp';
 import SignUpPage from './pages/signup/SignUpPage';
-import Main from './pages/main/Main';
+import Footer from './component/footer/Footer';
 
 import theme from './styles/theme';
-
-import Nav from './component/nav/Nav';
 
 const Router = (props) => {
   return (
@@ -23,6 +24,7 @@ const Router = (props) => {
           <Route path='/signup-check' element={<PhoneCheck />} />
           <Route path='/signup-page' element={<SignUpPage />} />
         </Routes>
+        <Footer />
       </BrowserRouter>
     </ThemeProvider>
   );
