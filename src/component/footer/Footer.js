9191be2f44e9a0a4f695c08d0e882a5381c42bdd
YesunPark <lynn08082@gmail.com>
2022-09-21 18:06:46 +0900

    Add: footer rayout

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,7 +1,27 @@
 import React from 'react';
+import styled from 'styled-components';
+
+const FooterStyle = styled.footer`
+  position: fixed;
+  bottom: 0px;
+  width: 100vw;
+  margin-top: 120px;
+  border: 1px solid black;
+
+  div {
+    padding: 60px 30px;
+    background-color: ${({ theme }) => theme.colors.greyBg};
+    color: ${({ theme }) => theme.colors.text};
+    text-align: center;
+  }
+`;
 
 const Footer = () => {
-  return;
+  return (
+    <FooterStyle>
+      <div>footer</div>
+    </FooterStyle>
+  );
 };
 
 export default Footer;
