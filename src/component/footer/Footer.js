26bb5cbce3f438eb353e902eb71b0851458fcda5
YesunPark <lynn08082@gmail.com>
2022-09-22 12:07:03 +0900

    Add: 페북, 블로그 아이콘 추가 중간과정

--- src/component/footer/Footer.js
+++ src/component/footer/Footer.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import styled from 'styled-components';
 
@@ -84,6 +85,7 @@ const Footer = () => {
 
           <div>Copyright JGUD COMPANY Corp. All rights reserved.</div>
         </div>
+        <div>{/* <FontAwesomeIcon icon={faFacebook} /> */}</div>
       </div>
     </FooterStyle>
   );
