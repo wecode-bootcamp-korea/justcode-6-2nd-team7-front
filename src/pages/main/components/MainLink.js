ff98982f4092d8639a7822ec9ea137a979c80f3e
YesunPark <lynn08082@gmail.com>
2022-09-23 19:58:43 +0900

    Fix: 링크 연결한 부분 삭제

--- src/pages/main/components/MainLink.js
+++ src/pages/main/components/MainLink.js
@@ -34,49 +34,41 @@ const MainLink = () => {
       id: 1,
       name: '모텔',
       eng: 'motel',
-      url: '/accommodation/모텔',
     },
     {
       id: 2,
       name: '호텔·리조트',
       eng: 'hotel',
-      url: '/accommodation/호텔·리조트',
     },
     {
       id: 3,
       name: '펜션',
       eng: 'pension',
-      url: '/accommodation/펜션',
     },
     {
       id: 4,
       name: '게스트하우스',
       eng: 'ghestHouse',
-      url: '/accommodation/게스트하우스',
     },
     {
       id: 5,
       name: '캠핑·글램핑',
       eng: 'camping',
-      url: '/accommodation/캠핑',
     },
     {
       id: 6,
       name: '한옥',
       eng: 'koreanHouse',
-      url: '/accommodation/한옥',
     },
     {
       id: 7,
       name: '내주변',
       eng: 'mySpace',
-      url: '/mySpace',
     },
     {
       id: 8,
       name: '페이백',
       eng: 'payBack',
-      url: '/',
     },
   ];
 
