32103974417a2e241fd5b95145aadd9fa7528724
yuna <yureve5772@gmail.com>
2022-09-27 03:44:25 +0900

    Add: nesting 수정하는 중

--- src/pages/detail/Detail.js
+++ src/pages/detail/Detail.js
@@ -99,7 +99,7 @@ const Detail = () => {
             ) : component === 2 ? (
               <Information informations={showData} />
             ) : (
-              <Review review={showData} />
+              <Review showData={showData} />
             )}
           </S.Context>
         </S.DetailExplanation>
--- src/pages/detail/components/Review.stlyed.js
+++ src/pages/detail/components/Review.stlyed.js
@@ -5,33 +5,12 @@ export const ReviewContainer = styled.div`
     padding: 44px 0;
     text-align: center;
   }
-
   .review-style {
     margin-top: 22px;
     font-size: 18px;
     padding-bottom: 10px;
     border-bottom: 1px solid ${({ theme }) => theme.colors.greyBg};
   }
-  .score-total {
-    display: flex;
-    justify-content: center;
-    .star-total {
-      width: 203px;
-      height: 36px;
-      background: no-repeat url('/images/detail/star.png');
-      background-size: 328px;
-      overflow: hidden;
-      background-position: 4px ${(props) => props.score}px;
-    }
-    /* .one {
-      background-position: 4 -45px;
-    } */
-  }
-  .score-num {
-    margin-top: 4px;
-    font-size: 24px;
-  }
-
   .general-review {
     display: block;
     padding: 0 0 10px 0;
@@ -39,12 +18,24 @@ export const ReviewContainer = styled.div`
     font-weight: normal;
     text-align: center;
   }
-
-  .img-size {
-    width: 56px;
-    height: 56px;
-    margin: 30px;
+  .score-total {
+    display: flex;
+    justify-content: center;
   }
+  .star-total {
+    width: 203px;
+    height: 36px;
+    background: no-repeat url('/images/detail/star.png');
+    background-size: 328px;
+    overflow: hidden;
+    background-position: 4px ${(props) => props.score}px;
+  }
+
+  .score-num {
+    margin-top: 4px;
+    font-size: 24px;
+  }
+
   .review-flex {
     display: flex;
     justify-content: space-between;
@@ -55,19 +46,6 @@ export const ReviewContainer = styled.div`
       padding-left: 30px;
       .name-flex {
         display: flex;
-      }
-    }
-    .score {
-      margin-top: 10px;
-      display: flex;
-      .star {
-        width: 122px;
-        height: 22px;
-        background: no-repeat url('/images/detail/star.png');
-        background-size: 180px;
-        overflow: hidden;
-        /* background-position: 4px ${(props) => props.score}px; */
-        /* background-position: 4px 3px; */
       }
     }
 
@@ -81,8 +59,30 @@ export const ReviewContainer = styled.div`
       padding-bottom: 20px;
     }
   }
-  .image-size {
-    width: 388px;
-    height: 288px;
+  .showData-flex {
+    display: flex;
+    margin: 30px;
+    .icon-size {
+      width: 56px;
+      height: 56px;
+      margin: 30px;
+    }
+    .image-size {
+      width: 388px;
+      height: 288px;
+    }
+    .score {
+      margin-top: 10px;
+      display: flex;
+    }
+    .star {
+      width: 122px;
+      height: 22px;
+      background: no-repeat url('/images/detail/star.png');
+      background-size: 180px;
+      overflow: hidden;
+      /* background-position: 4px ${(props) => props.score}px; */
+      background-position: 4px 3px;
+    }
   }
 `;
