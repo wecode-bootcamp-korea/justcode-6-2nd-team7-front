d50932d76118758f5649d8ad9124a17dd0d8d360
yuna <yureve5772@gmail.com>
2022-09-26 16:10:57 +0900

    Fix: 오류수정

--- src/pages/detail/components/map.js
+++ src/pages/detail/components/map.js
@@ -4,21 +4,26 @@ import styled from 'styled-components';
 const { kakao } = window;
 
 const Main = styled.div`
+  // 원하는 css 스타일링
   margin: 0 auto;
   width: 848px;
   height: 160px;
   .mapDetail {
+    // 원하는 css 스타일링
     width: 100%;
     height: 100%;
   }
 `;
 
 const Map = ({ list, map }) => {
+  console.log('나와라', map);
   useEffect(() => {
     const container = document.getElementById('map');
     const options = {
       center: new kakao.maps.LatLng(map.lat, map.lng),
       level: 3,
+      // center: new kakao.maps.LatLng(37.504897, 127.04961),
+      // level: 6,
     };
     const kakaoMap = new kakao.maps.Map(container, options);
     const marker = new kakao.maps.Marker({
