c34e62ee3e635673920fbbb4e2fd2020a55f8ee9
YesunPark <lynn08082@gmail.com>
2022-09-26 16:47:54 +0900

    Add: 검색페이지 get api 임시작성

--- src/pages/search/Search.js
+++ src/pages/search/Search.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 import { useRecoilState } from 'recoil';
 import { searchInputState } from '../../atom';
 import styled from 'styled-components';
+import { useState } from 'react';
 
 const SearchContainer = styled.div`
   header {
@@ -34,11 +35,25 @@ const SearchContainer = styled.div`
 
 const Search = () => {
   const [keyword] = useRecoilState(searchInputState);
+  const [searchList, setSearchList] = useState([]);
+
+  // 추후 원희님 필터/카드 컴포넌트 추가되면 수정될 수 있음
+  // useEffect(() => {
+  //   axios
+  //     .get('/data/accommodation/accommodation.json')
+  //     //   .get(`http://localhost:8000/accommodation/${param}`)
+  //     .then((res) => {
+  //       setList(res.data.accommodation);
+  //       Object.keys(res.data.accommodation).length === 0 && setList([]);
+  //     })
+  //     .catch((err) => {
+  //       console.log(err);
+  //       setList([]);
+  //     });
+  // }, []);
 
   //const param = useParams().type; => SideFilter, TopFilet등 가져와야 하는데
   //원희님의 도움이 필요할 것 같아요 ㅠ 라이브러리 복잡혀..
-
-  //검색어에 해당하는 숙소정보 가져와야함. 수정예정
 
   return (
     <SearchContainer>
