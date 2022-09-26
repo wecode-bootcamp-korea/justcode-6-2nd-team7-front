cdc1a4cead9b2f501a8b01cb04349afee279f819
yuna <yureve5772@gmail.com>
2022-09-26 20:02:19 +0900

    Add: 리뷰 구현중

--- src/pages/detail/components/Review.js
+++ src/pages/detail/components/Review.js
@@ -1,27 +1,45 @@
 import * as S from './Review.stlyed';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faStar } from '@fortawesome/free-solid-svg-icons';
+import { version } from 'react';
 
 const Review = ({ review }) => {
-  console.log('리뷰의평점', review);
   return (
     <S.ReviewContainer>
       <div>
-        <div>추천해요</div>
-        <div>
-          <span>별점</span>
-          <span>{review.score}</span>
+        <div className='score-top'>
+          <div className='general-review'>추천해요</div>
+          <div className='score'>
+            <span>
+              <FontAwesomeIcon icon={faStar} color='#ffcc00' />
+            </span>
+            <span>{review.score}</span>
+          </div>
+          <div className='review-style'>
+            <span>전체리뷰</span>
+            <span>{review.review}</span>
+          </div>
         </div>
-        <div>
-          <span>전체리뷰</span>
-          <span>{review.review}</span>
-        </div>
-        <div>
+        <div className='name-flex'>
           <div>
             <img className='img-size' src='https://image.goodchoice.kr/profile/ico/ico_24.png' />
           </div>
-          <div>여기라면 가요</div>
+          <div>
+            <div>여기라면 가요</div>
+            <div>
+              <FontAwesomeIcon icon={faStar} color='#ffcc00' />
+              {review.comment[0].score}
+            </div>
+
+            <div className='name-flex'>
+              <div>{review.comment[0].roomType}</div>
+              <div>{review.comment[0].nickName}</div>
+            </div>
+            <div>{review.comment[0].comment}</div>
+            <div>{review.comment[0].url}</div>
+          </div>
         </div>
       </div>
-      <div></div>
     </S.ReviewContainer>
   );
 };
--- src/pages/detail/components/Review.stlyed.js
+++ src/pages/detail/components/Review.stlyed.js
@@ -1,8 +1,37 @@
 import styled from 'styled-components';
 
 export const ReviewContainer = styled.div`
+  .score-top {
+    padding: 44px 0;
+    text-align: center;
+  }
+  .general-review {
+    display: block;
+    padding: 0 0 10px 0;
+    font-size: 24px;
+    font-weight: normal;
+    text-align: center;
+  }
   .img-size {
     width: 56px;
     height: 56px;
+  }
+  .name-flex {
+    display: flex;
+  }
+  .score {
+    display: inline-block;
+    position: relative;
+    top: -1px;
+    padding-left: 12px;
+    font-size: 32px;
+    line-height: 1;
+    vertical-align: top;
+  }
+  .review-style {
+    margin-top: 22px;
+    font-size: 18px;
+    padding-bottom: 10px;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.greyBg};
   }
 `;
