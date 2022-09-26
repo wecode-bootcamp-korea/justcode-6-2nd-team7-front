7d2e838de7b52f35df2cc6dfc843cd3d3a12cb22
yuna <108171986+yoon1257@users.noreply.github.com>
2022-09-26 13:32:46 +0900

    Feature/detail map (#28)
    * Add: 숙소정보 구현

--- src/pages/detail/components/Information.js
+++ src/pages/detail/components/Information.js
@@ -1,4 +1,57 @@
-const Information = () => {
-  return <>숙소정보입니다.</>;
+import styled from 'styled-components';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faAngleDown } from '@fortawesome/free-solid-svg-icons';
+import { useState } from 'react';
+import AccommodationInfo from './AccommdationInfo';
+const Info = styled.div`
+  .info-flex {
+    display: flex;
+    align-items: center;
+  }
+  .information {
+    width: 100%;
+    padding: 30px;
+    text-align: left;
+    font-size: 18px;
+    border: none;
+    background-color: transparent;
+    border-bottom: 1px solid ${({ theme }) => theme.colors.greyBg};
+    cursor: pointer;
+  }
+`;
+
+const Information = ({ informations }) => {
+  const [accInfo, setAccInfo] = useState(false);
+  const [basicInfo, setBasicInfo] = useState(false);
+
+  const handleInfoClick = () => {
+    setAccInfo((accInfo) => !accInfo);
+  };
+  const handleBasicClick = () => {
+    setBasicInfo((basicInfo) => !basicInfo);
+  };
+
+  return (
+    <Info>
+      <div className='info-flex'>
+        <button className='information' onClick={handleInfoClick}>
+          숙소정보
+        </button>
+        <FontAwesomeIcon icon={faAngleDown} />
+      </div>
+      <div>
+        accInfo === true ? <AccommodationInfo accommodation={informations} />
+      </div>
+      <div className='info-flex'>
+        <button className='information' onClick={handleBasicClick}>
+          기본정보
+        </button>
+        <FontAwesomeIcon icon={faAngleDown} />
+      </div>
+      <div>
+        basicInfo === true ? <AccommodationInfo />{' '}
+      </div>
+    </Info>
+  );
 };
 export default Information;
