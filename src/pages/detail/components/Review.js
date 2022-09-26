d57616cfdb23803529b9e2f1f178bebb472292b2
yuna <yureve5772@gmail.com>
2022-09-26 17:32:53 +0900

    Add: 리뷰 구현중

--- src/pages/detail/components/Review.js
+++ src/pages/detail/components/Review.js
@@ -1,9 +1,28 @@
+import * as S from './Review.stlyed';
+
 const Review = ({ review }) => {
   console.log('리뷰의평점', review);
   return (
-    <>
+    <S.ReviewContainer>
+      <div>
+        <div>추천해요</div>
+        <div>
+          <span>별점</span>
+          <span>{review.score}</span>
+        </div>
+        <div>
+          <span>전체리뷰</span>
+          <span>{review.review}</span>
+        </div>
+        <div>
+          <div>
+            <img className='img-size' src='https://image.goodchoice.kr/profile/ico/ico_24.png' />
+          </div>
+          <div>여기라면 가요</div>
+        </div>
+      </div>
       <div></div>
-    </>
+    </S.ReviewContainer>
   );
 };
 
---
+++ src/pages/detail/components/Review.stlyed.js
@@ -0,0 +1,8 @@
+import styled from 'styled-components';
+
+export const ReviewContainer = styled.div`
+  .img-size {
+    width: 56px;
+    height: 56px;
+  }
+`;
