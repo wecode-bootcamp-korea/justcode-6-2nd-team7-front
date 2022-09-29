237cf085285b6a94b05309942c4ac736d9577020
YesunPark <108933466+YesunPark@users.noreply.github.com>
2022-09-29 10:18:26 +0900

    Fix: 예약페이지에서 유효성검사 기준에 따라 생성되는 모달의 내용 변경 (#55)
    * Fix: 결제하기 버튼 클릭 시 모달창 구현
    
    * Modify: 예약페이지 상태값 큰 컨테이너에서 관리
    
    * Refactor: 모달배경 공통색으로 수정
    
    * Fix: 예약페이지 input값들의 invalid에 따라 결제하기 모달 변화
    
    * Add: 예약완료 후 메인페이지로 이동
    
    * Refactor: 조건부 랜더링 사용으로 코드 축소

--- src/pages/reservation/Reservation.js
+++ src/pages/reservation/Reservation.js
@@ -1,13 +1,43 @@
+import { useEffect, useState } from 'react';
 import Main from './components/Main';
 import Sub from './components/Sub';
 
 import { ReservationStyle } from './Reservation.Styled';
 
 const Reservation = () => {
+  const [checkList, setCheckList] = useState([]);
+  const [nameValid, setNameValid] = useState(false);
+  const [phoneValid, setPhoneValid] = useState(false);
+  const [modalMsg, setModalMsg] = useState('');
+
+  useEffect(() => {
+    !nameValid
+      ? setModalMsg('예약자 이름을 확인해주세요.')
+      : !phoneValid
+      ? setModalMsg('휴대폰번호를 확인해주세요.')
+      : checkList.length !== 3
+      ? setModalMsg('필수동의 항목을 확인하세요.')
+      : setModalMsg('결제하시겠습니까?');
+  }, [nameValid, phoneValid, checkList]);
+
+  useEffect(() => {
+    if (localStorage.getItem('token') !== null) {
+      setNameValid(true);
+      setPhoneValid(true);
+    }
+  }, []);
+
   return (
     <ReservationStyle>
-      <Main />
-      <Sub />
+      <Main
+        checkList={checkList}
+        setCheckList={setCheckList}
+        nameValid={nameValid}
+        setNameValid={setNameValid}
+        phoneValid={phoneValid}
+        setPhoneValid={setPhoneValid}
+      />
+      <Sub modalMsg={modalMsg} />
     </ReservationStyle>
   );
 };
